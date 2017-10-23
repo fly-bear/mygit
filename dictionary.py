c82d4a250103c8d82756d3337e0a976aa1fefffd
flybear <dzx518@qq.com>
2017-10-23 11:21:40 +0800

    2017-10-23-11:21

--- dictionary.py
+++ dictionary.py
@@ -2,26 +2,58 @@ import hashlib
 import requests
 import random
 
-appid = '20171022000090226'
-secretKey = 'S3r7aSwezUsDE8pauoeN'
+appid = '01e625e747188841'
+appKey = 'X6OyKvqaBz6XlQ0Nk58iLpUP3R9Hour1'
 
- 
+para={'q':'','from':'','to':'','appKey':appid,'salt':'','sign':''}
 httpClient = None
-myurl = 'http://api.fanyi.baidu.com/api/trans/vip/translate'
+myurl = 'http://openapi.youdao.com/api'
 while True:
 	q = input()
 	if ord(q[0])not in range(97,122) and ord(q[0]) not in range(65,90):
-		fromLang = 'zh'
+		fromLang = 'zh-CHS'
 		toLang = 'en'
 	else:
 		fromLang = 'en'
-		toLang = 'zh'
+		toLang = 'zh-CHS'
 	salt = random.randint(32768, 65536)
-	sign = appid+q+str(salt)+secretKey
+	sign = appid+q+str(salt)+appKey
 	m1 = hashlib.md5()
 	m1.update(sign.encode('utf-8'))
 	sign = m1.hexdigest()
-	myurl = myurl+'?appid='+appid+'&q='+q+'&from='+fromLang+'&to='+toLang+'&salt='+str(salt)+'&sign='+sign
-	pass
-	a=requests.get(myurl).json()
-	print('\n'+a['trans_result'][0]['dst']+'\n\n---------------------------')
+	#myurl = myurl+'?appid='+appid+'&q='+q+'&from='+fromLang+'&to='+toLang+'&salt='+str(salt)+'&sign='+sign
+	para['from']=fromLang
+	para['to']=toLang
+	para['q']=q
+	para['salt']=salt
+	para['sign']=sign
+	a=requests.get(myurl,params=para).json()
+	print(a)
+	print('\n')
+	for temp in a['translation']:
+		print(temp)
+	print('\n')
+	try:
+		for temp in a['web']:
+			for ttemp in temp['value']:
+				print(ttemp,end=',')
+			print('['+temp['key']+']')
+	except:
+		pass
+	print('\n')
+	try:
+		temp = a['basic']
+		for ttemp in temp['explains']:
+			print(ttemp)
+	except:
+		pass
+	print('\n')
+	try:
+		if fromLang =='zh-CHS':
+			print('拼音：'+temp['phonetic'])
+		else:
+			print('英式音标：'+temp['uk-phonetic'])
+			print('美式音标：' + temp['us-phonetic'])
+	except:
+		pass
+	print('-'*100)
