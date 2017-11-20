1028ed47e8125a7bf87d4ed4caad8e7927c426a6
flybear <dzx518@qq.com>
2017-11-20 16:33:48 +0800

    none

--- tcp_client.py
+++ tcp_client.py
@@ -1,25 +1,27 @@
 import socket
 import sys
+import time
 
-target_host='106.15.199.206'
-target_port=5566
+target_host='localhost'
+target_port=6666
 
-# client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-# client.connect((target_host,target_port))
+client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client.connect((target_host,target_port))
 while True:
     data=input('>>')
     if not data:
         continue
     elif data=='exit':
+        client.send('close_connect'.encode())
         break
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((target_host, target_port))
+    # client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # client.connect((target_host, target_port))
     client.send(data.encode())
+    data=''
     while True:
-        data=client.recv(4096).decode()
-        if data:
-            print(data)
-        else:
+        data=data+client.recv(4096).decode()
+        if data[-13:]=='SEND_finished':
             break
+    print(data[:-13])
     # client.close()
 client.close()
--- tcp_server.py
+++ tcp_server.py
@@ -14,15 +14,25 @@ server.listen(5)
 print('[*] Listening on %s:%d' % (bind_ip,bind_port))
 
 def handle_client(client_socket):
-    request = client_socket.recv(4096)
-    output=subprocess.check_output(request.decode(),stderr=subprocess.STDOUT,shell=True)
-    print('[*] Recieved: %s'% request.decode())
-    try:
-        client_socket.send((output.decode(encoding='utf-8')).encode())
-    except UnicodeDecodeError as e:
-        client_socket.send((output.decode(encoding='gbk')).encode())
-
+    while True:
+        request = client_socket.recv(4096)
+        if request.decode()=='close_connect':
+            break
+        print('[*] Recieved: %s' % request.decode())
+        try:
+            output=subprocess.check_output(request.decode(),stderr=subprocess.STDOUT,shell=True)
+            try:
+                client_socket.send((output.decode(encoding='utf-8')).encode())
+                client_socket.send('SEND_finished'.encode())
+            except UnicodeDecodeError as e:
+                client_socket.send((output.decode(encoding='gbk')).encode())
+                client_socket.send('SEND_finished'.encode())
+        except subprocess.CalledProcessError as e:
+            client_socket.send('无此命令！'.encode())
+            client_socket.send('SEND_finished'.encode())
+        # client_socket.close()
     client_socket.close()
+    print('diconnect')
 
 while True:
     client,addr = server.accept()
